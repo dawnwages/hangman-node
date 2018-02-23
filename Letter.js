69d0188249b460a2a2a031bfea930b3016137c0e
Dawn Wages <dawn.wages@gmail.com>
2018-02-22 19:32:10 -0500

    arraying my hangman

--- Letter.js
+++ Letter.js
@@ -5,28 +5,40 @@
 //fullWord = the word that the user is guessing against. This should be designed in a different document
 
 var lowerFullWord = "Banana Pancakes";
-var fullWord = lowerFullWord.toUpperCase();
+var fullWord = lowerFullWord.toUpperCase().split("");
+//var wordArray = fullWord.split("");
 var userGuess = process.argv[2].toUpperCase();
+
+// if(fullWord.indexOf(this.letterGuess) > -1){
+console.log(fullWord);
 
 function Letter(letterGuess, ifAccurate, letterSpace){ 
     this.letterGuess = letterGuess;
-    this.ifAccurate = false;
+    this.ifAccurate = 0;
     this.letterSpace = "_";
 
     //function that returns the underlying character if the letter has been guessed or a placeholder if the letter has not been guessed
     this.letterGame = function() {
-        if(fullWord.indexOf(this.letterGuess) > -1){
-            console.log("correct!");
-            this.ifAccurate = true;
-            this.letterSpace = userGuess;
-            console.log(this.ifAccurate);
-            console.log(this.letterSpace);
-        } else {
-            console.log("this letter is not in the word");
-            this.ifAccurate = false;
-            console.log(this.ifAccurate);
-            console.log(this.letterSpace);
+        var letterCount = 0;
+        for( x = 0; x < fullWord.length; x++){    
+            if(this.letterGuess === fullWord[x]) {
+            
+                console.log("correct!");
+                var letterLocation = x;
+                letterCount++
+                console.log(letterLocation);
+                this.ifAccurate = 1;
+                this.letterSpace = userGuess;
+                console.log(this.ifAccurate);
+                console.log(this.letterSpace);
+            } else {
+                //console.log("this letter is not in the word");
+                this.ifAccurate = 0;
+                //console.log(this.ifAccurate);
+                //console.log(this.letterSpace);
+            }
         }
+        console.log("you have matched "+letterCount+" letter(s)!");
     }
 
 }
