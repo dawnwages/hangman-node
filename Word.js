0e19861bd480a734c90c0c04506929c96831477c
Dawn Wages <dawn.wages@gmail.com>
2018-02-27 23:19:41 -0500

    Updating Word. I still need to figure out how to get the next word to repeat. I think it's because I need to have function with inquirer on index.js but how do I get to emptyWord? I think I need to return empty word.

--- Word.js
+++ Word.js
@@ -5,6 +5,37 @@ const inquirer = require("inquirer");
 var emptyWord = [];
 var wordLength = 0;
 
+var updateCheckLetter = function () {
+    var c = 0;// c will need to be changed to word length, but this is just to keep it under control for now.
+    inquirer.prompt([     
+        {
+                    name: 'letterCharacter',
+                    message: 'Guess a letter!',
+        }
+    ]).then(function(answers){
+        console.log("You picked :"+answers.letterCharacter);
+        for (l = 0; l < emptyWord.length; l++){
+            if(emptyWord[l].letterCharacter === answers.letterCharacter){
+                emptyWord[l].letterPicked = true;
+                emptyWord[l].displayLetter = answers.letterCharacter;
+                wordLength--;
+               // console.log("updated letter Character: "+answers.letterCharacter);
+               // console.log("updated display letter: "+emptyWord[l].displayLetter);
+            } else {
+               // console.log(emptyWord[l].letterCharacter+" did not match");
+            }  
+        }
+       console.log("line 63 this should be all of the letter objects: "+emptyWord.join(" "));        
+       if(wordLength > 0){ 
+           updateCheckLetter();
+       } else {
+           console.log('/n'+"Congratulations! You've won the game!!"+'/n');
+       }
+                
+    });
+
+};
+
 var Word = function(wordChoice){
     this.wordChoice = wordChoice;
     this.fullWord = wordChoice.toUpperCase().split("");
@@ -37,63 +68,18 @@ var Word = function(wordChoice){
     this.createLetters();
     //this.updateCheckLetter(); // No longer need this because we are taking this out of the constructor -- I don't think we have any need for "this" either. We want to keep it on this page so we can communicate with the letter constructor. I guess we can call it on the index page maybe?
         
-      
-        // var g = 0;
-        // if (g < 5){
-        //     g++;
-        //     console.log(g+" chances")
-        //    // this.updateCheckLetter();
-        // } else {
-        //     console.log("Your turn is over");
-        // }
-      
-        //this.updateCheckLetter();
+    if (emptyWord.length === 0){
+        console.log("Start the Hangman Game!");
+    } else {
+        console.log("line 74 this should be all of the letter objects: "+emptyWord.join(" "));
+    } 
 
-}
+        updateCheckLetter();
 
-
-if (emptyWord.length === 0){
-    console.log("Start the Hangman Game!");
-} else {
-    console.log("line 74 this should be all of the letter objects: "+emptyWord.join(" "));
-}
-
-var updateCheckLetter = function () {
-    var c = 0;// c will need to be changed to word length, but this is just to keep it under control for now.
-    inquirer.prompt([     
-        {
-                    name: 'letterCharacter',
-                    message: 'Guess a letter!',
-        }
-    ]).then(function(answers){
-        console.log("You picked :"+answers.letterCharacter);
-        for (l = 0; l < emptyWord.length; l++){
-            if(emptyWord[l].letterCharacter === answers.letterCharacter){
-                emptyWord[l].letterPicked = true;
-                emptyWord[l].displayLetter = answers.letterCharacter;
-                console.log("updated letter Character: "+answers.letterCharacter);
-                console.log("updated display letter: "+emptyWord[l].displayLetter);
-            } else {
-                console.log(emptyWord[l].letterCharacter+" did not match");
-            }  
-        }
-       console.log("line 63 this should be all of the letter objects: "+emptyWord.join(" "));        
-       if(c < 3){
-           c++;
-           updateCheckLetter();
-       } else {
-           console.log("done");
-       }
-                
-    });
-
-    // for ( l = 0; l < emptyWord.length; l++){
-    //     //console.log(emptyWord[l].displayLetter);
-    //     console.log(emptyWord[l].letterCharacter);
-    // }
 };
 
 
-updateCheckLetter();
+
+
 
 module.exports = Word;
